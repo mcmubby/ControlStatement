a7334ea720fc238d84c7e33040add35aba77d4a3
Mubarak <12859241+mcmubby@users.noreply.github.com>
2021-04-20 12:51:25 +0100

    switch case

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace ControlStatement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            int number = 10;
+
+        //     switch (number)
+        //     {
+        //         case 10:
+        //         Console.WriteLine("Number is 10");
+        //         break;
+        //         case 20:
+        //         Console.WriteLine("Number is 20");
+        //         break;
+        //         case 30:
+        //         Console.WriteLine("Number is 30");
+        //         break;
+        //         default:
+        //         Console.WriteLine("Invalid number");
+        //         break;
+        //     }
+        // }
     }
 }
